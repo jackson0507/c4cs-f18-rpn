592e0b0ee7a0f492f9944151f2c4fd545e1488e1
jackmcin <jackmcin@umich.edu>
2018-11-07 23:59:54 -0500

    fixed exponent test

--- test_rpn.py
+++ test_rpn.py
@@ -15,5 +15,5 @@ class TestBasics(unittest.TestCase):
             result = rpn.calculate("1 1 3 +")
 
     def test_exponent(self):
-        result = rpn.calculate(2 3 ^)
+        result = rpn.calculate("2 3 ^")
         self.assertEqual(8, result)
